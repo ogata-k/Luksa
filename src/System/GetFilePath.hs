39c6fa3b7073f82f4411da9d5ad7b0ac44090c24
ogata-k <ogtkzk@yahoo.co.jp>
2018-01-03 19:00:47 +0900

    impl getLuksaConfigDir

--- src/System/GetFilePath.hs
+++ src/System/GetFilePath.hs
@@ -1,21 +1,26 @@
 module System.GetFilePath where
 
 --Windows専用
-import System.FilePath.Windows (takeDirectory
-                                , doesDirectoryExist
-                                )
-import System.Environment (getExecutablePath)
+import System.FilePath.Windows
+import System.Directory
+import System.Environment
 
--- 以下、Luksaの実行ファイルからファイルパスをを取得する関数
+-- 以下、Luksaの実行ファイルからファイルパスを取得する関数
 getLuksaDir :: IO (Maybe FilePath)
 getLuksaDir = do
     exePath <- getExecutablePath
-    dir <- takeDirectory exePath
+    let dir = takeDirectory exePath
     existFlg <- doesDirectoryExist dir
-    if existFlg then return $ Just dir else return Nothing
+    return $ if existFlg then Just dir else Nothing
 
-getLuksaConfigDir :: Maybe FilePath
-getLuksaConfigDir = undefined
+getLuksaConfigDir:: IO (Maybe FilePath)
+getLuksaConfigDir = do
+    dir <- getLuksaDir
+    let filePath = do fmap (</> "LuksaConfig") dir
+    existFlag <- case filePath of
+                    Just path -> doesDirectoryExist path
+                    Nothing -> return False
+    return $ if existFlag then filePath else Nothing
 
 getTemplatesDir :: Maybe FilePath
 getTemplatesDir = undefined
