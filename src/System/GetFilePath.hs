0b4827f3dfce591bb39b53848fc6f84ac0071101
ogata-k <ogtkzk@yahoo.co.jp>
2018-01-03 14:43:32 +0900

    make stub in GetFilePath.hs

--- src/System/GetFilePath.hs
+++ src/System/GetFilePath.hs
@@ -1,2 +1,40 @@
 module System.GetFilePath where
 
+import System.FilePath.Windows  --Windows専用
+import System.Environment
+
+-- 以下、Luksaの実行ファイルからファイルパスをを取得する関数
+getLuksaExeFile :: Maybe FilePath
+getLuksaExeFile = undefined
+
+getLuksaConfigDir :: Maybe FilePath
+getLuksaConfigDir = undefined
+
+getTemplatesDir :: Maybe FilePath
+getTemplatesDir = undefined
+
+getTemplateDir :: String -> Maybe FilePath
+getTemplateDir = undefined
+
+-- 以下、プロジェクトに関するファイルパスを取得する関数
+getProjectDir :: Maybe FilePath
+getProjectDir = undefined
+
+getDocumentDir :: Maybe FilePath
+getDocumentDir = undefined
+
+getMainLkFile :: Maybe FilePath
+getMainLkFile = undefined
+
+getHelperDir :: Maybe FilePath
+getHelperDir = undefined
+
+getImageDir :: Maybe FilePath
+getImageDir = undefined
+
+getConvertedDir :: Maybe FilePath
+getConvertedDir = undefined
+
+getProjectYamlFile :: Maybe FilePath
+getProjectYamlFile = undefined
+
