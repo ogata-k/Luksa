ce907c765c0d71c0da01fc4895d3f24bf04127e3
ogata-k <ogtkzk@yahoo.co.jp>
2018-01-04 12:06:30 +0900

    add comment and impl relative LuksaDir func

--- src/System/GetFilePath.hs
+++ src/System/GetFilePath.hs
@@ -5,28 +5,45 @@ import System.FilePath.Windows
 import System.Directory
 import System.Environment
 
--- 以下、Luksaの実行ファイルからファイルパスを取得する関数
+-- MaybeなFilePathから取得
+takeMaybeFilePath :: Maybe FilePath -> String
+takeMaybeFilePath path = 
+    case path of
+        Just f -> f
+        Nothing -> ""
+
+-- 以下、Luksaの実行ファイルからファイルパスを取得する関数 ----------
+-- luksa.exeが存在する親ディレクトリを取得
 getLuksaDir :: IO (Maybe FilePath)
 getLuksaDir = do
     exePath <- getExecutablePath
     let dir = takeDirectory exePath
     existFlg <- doesDirectoryExist dir
-    return $ if existFlg then Just dir else Nothing
-
-getLuksaConfigDir:: IO (Maybe FilePath)
-getLuksaConfigDir = do
+    exeExistFlg <- doesFileExist exePath
+    return $ if existFlg && exeExistFlg then Just dir else Nothing
+-- luksa.exeの親ディレクトリから子供を取得
+getLuksaDirChildDir, getLuksaDirChildFile :: String -> IO (Maybe FilePath)
+getLuksaDirChildDir targetDir = do
     dir <- getLuksaDir
-    let filePath = do fmap (</> "LuksaConfig") dir
+    let filePath = do fmap (</> targetDir) dir
+    putStrLn $ "search : " ++ takeMaybeFilePath filePath
     existFlag <- case filePath of
                     Just path -> doesDirectoryExist path
                     Nothing -> return False
     return $ if existFlag then filePath else Nothing
 
-getTemplatesDir :: Maybe FilePath
-getTemplatesDir = undefined
-
-getTemplateDir :: String -> Maybe FilePath
-getTemplateDir = undefined
+getLuksaDirChildFile targetFile = do
+    dir <- getLuksaDir
+    let filePath = do fmap (</> targetFile) dir
+    putStrLn $ "search : " ++ takeMaybeFilePath filePath
+    existFlag <- case filePath of
+                    Just path -> doesFileExist path
+                    Nothing -> return False
+    return $ if existFlag then filePath else Nothing
+-- それぞれ特殊なファイルようの取得関数
+getLuksaConfigDir = getLuksaDirChildDir "LuksaConfig"
+getTemplatesDir = getLuksaDirChildDir $ "LuksaConfig" </> "templates"
+getTemplateDir template = getLuksaDirChildDir $ "LuksaConfig" </> "templates" </> template
 
 -- 以下、プロジェクトに関するファイルパスを取得する関数
 getProjectDir :: Maybe FilePath
