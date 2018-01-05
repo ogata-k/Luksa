b25ebcd3e13b654de80a24aefefbdeceb0206053
ogata-k <ogtkzk@yahoo.co.jp>
2018-01-05 22:04:55 +0900

    impl get child for homeDir
    generization of get child File and child Directory

--- src/System/GetFilePath.hs
+++ src/System/GetFilePath.hs
@@ -6,12 +6,46 @@ import System.Directory
 import System.Environment
 import Data.List (foldl')
 
--- MaybeなFilePathから取得
+-- 以下、一般的な取得関数
 takeMaybeFilePath :: Maybe FilePath -> String
 takeMaybeFilePath path = 
     case path of
         Just f -> f
         Nothing -> ""
+
+getChildDir, getChildFile :: IO (Maybe FilePath)
+                             -> String
+                             -> IO (Maybe FilePath)
+getChildDir getParent targetDir = do
+    dir <- getParent
+    let filePath = do fmap (</> targetDir) dir
+    putStrLn $ "search : " ++ takeMaybeFilePath filePath
+    existFlag <- case filePath of
+                    Just path -> doesDirectoryExist path
+                    Nothing -> return False
+    if existFlag
+        then do
+            putStrLn $ "find : " ++ takeMaybeFilePath filePath
+            return filePath
+        else do
+            putStrLn "can not find"
+            return Nothing
+
+getChildFile getParent targetFile = do
+    dir <- getParent
+    let filePath = do fmap (</> targetFile) dir
+    putStrLn $ "search : " ++ takeMaybeFilePath filePath
+    existFlag <- case filePath of
+                    Just path -> doesFileExist path
+                    Nothing -> return False
+    if existFlag
+        then do
+            putStrLn $ "find : " ++ takeMaybeFilePath filePath
+            return filePath
+        else do
+            putStrLn "can not find"
+            return Nothing
+
 
 -- 以下、Luksaの実行ファイルからファイルパスを取得する関数 ----------
 -- luksa.exeが存在する親ディレクトリを取得
@@ -21,35 +55,26 @@ getLuksaDir = do
     let dir = takeDirectory exePath
     existFlg <- doesDirectoryExist dir
     exeExistFlg <- doesFileExist exePath
-    return $ if existFlg && exeExistFlg then Just dir else Nothing
--- luksa.exeの親ディレクトリから子供を取得
-getLuksaDirChildDir, getLuksaDirChildFile :: String -> IO (Maybe FilePath)
-getLuksaDirChildDir targetDir = do
-    dir <- getLuksaDir
-    let filePath = do fmap (</> targetDir) dir
-    putStrLn $ "search : " ++ takeMaybeFilePath filePath
-    existFlag <- case filePath of
-                    Just path -> doesDirectoryExist path
-                    Nothing -> return False
-    return $ if existFlag then filePath else Nothing
+    if existFlg && exeExistFlg
+        then do
+            putStrLn $ "find LuksaDir : " ++ dir
+            return $ Just dir
+        else do
+            putStrLn "can not find LuksaDir"
+            return Nothing
 
-getLuksaDirChildFile targetFile = do
-    dir <- getLuksaDir
-    let filePath = do fmap (</> targetFile) dir
-    putStrLn $ "search : " ++ takeMaybeFilePath filePath
-    existFlag <- case filePath of
-                    Just path -> doesFileExist path
-                    Nothing -> return False
-    return $ if existFlag then filePath else Nothing
--- それぞれ特殊なファイルようの取得関数
+-- luksa.exeの親ディレクトリから子供を取得
+getLuksaDirChildDir = getChildDir getLuksaDir
+getLuksaDirChildFile = getChildFile getLuksaDir
 getLuksaConfigDir = getLuksaDirChildDir "LuksaConfig"
 getTemplatesDir = getLuksaDirChildDir $ "LuksaConfig" </> "templates"
 getTemplateDir template = getLuksaDirChildDir $ "LuksaConfig" </> "templates" </> template
 
+
 -- 以下、プロジェクトに関するファイルパスを取得する関数
 -- 最低限プロジェクトの構成は満たしているプロジェクトを取得
 getProjectDir :: IO (Maybe FilePath)
--- 現在のディレクトリを取得してその中にproject.yamlがなければ親でもう一度探す。あればdocument,main.lk,helper,imageがあるかを確認。それが通ればそのディレクトリがプロジェクトとする。
+-- 現在のディレクトリを取得してそこがプロジェクトと判定されれなければ親でもう一度探す。
 getProjectDir = do
     current <- getCurrentDirectory
     home <- getHomeDir
@@ -69,39 +94,33 @@ getProjectDir = do
         putStrLn $ "search directory : " ++ currentDir
         -- Luksaのプロジェクト作成コマンド的にプロジェクトの親ディレクトリはホームディレクトリにならないのでホームになったら打ち切り
         if currentDir == home
-            then return Nothing
+            then do
+                    putStrLn "can not find project"
+                    return Nothing
             else do
                 existFlg <- isProject currentDir
                 if existFlg
                     then do
-                        putStrLn $ "find : " ++ currentDir
+                        putStrLn $ "find project : " ++ currentDir
                         return $ Just currentDir
                     else do
                         let parent = takeDirectory currentDir
                         getProjectDir' home parent
-
+    -- document,main.lk,helper,imageがあるかを確認。それが通ればそのディレクトリがプロジェクト
     isProject :: FilePath -> IO Bool
     isProject target = do  -- targetは絶対パス
         children <- listDirectory target
-        let existFlg = foldl'(&&) True $ map (\f -> f children) $ map elem ["project.yaml", "document", "helper", "image"] -- ここは用編集
+        let existFlg = foldl'(&&) True $ map (\f -> f children) $ map elem ["project.yaml", "document", "helper", "image"]
         mainFileExist <- doesFileExist $ target </> "document" </> "main.lk"
-        return $ if existFlg && mainFileExist then True else False
+        return $ existFlg && mainFileExist
 
-getDocumentDir :: Maybe FilePath
-getDocumentDir = undefined
-
-getMainLkFile :: Maybe FilePath
-getMainLkFile = undefined
-
-getHelperDir :: Maybe FilePath
-getHelperDir = undefined
-
-getImageDir :: Maybe FilePath
-getImageDir = undefined
-
-getConvertedDir :: Maybe FilePath
-getConvertedDir = undefined
-
-getProjectYamlFile :: Maybe FilePath
-getProjectYamlFile = undefined
+-- projectの子供を取得する関数
+getProjectChildDir = getChildDir getProjectDir 
+getProjectChildFile = getChildFile getProjectDir
+getDocumentDir = getProjectChildDir "document"
+getMainLkFile = getProjectChildFile $ "document" </> "main.lk"
+getHelperDir = getProjectChildDir "helper"
+getImageDir = getProjectChildDir "image"
+getConvertedDir = getProjectChildDir "converted"
+getProjectYamlFile = getProjectChildFile "project.yaml"
 
